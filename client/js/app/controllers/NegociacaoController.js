9d58fc657d150e4442f283484064341f875df1a8
Cássio Antoniazzi <cassiosvaldo@gmail.com>
2016-09-03 22:53:06 -0300

    Criado views

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,23 +5,33 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputData = $('#data');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
+
+        this._negociacoesView = new NegociacaoView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
     }
 
     adiciona(event) {
+
         event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._limpaFormulario();
+    }
 
-        let data = new Date(
-            ...this._inputData.value
-            .split('-')
-            .map((item, index) => item - index % 2)
-        );
+    _criaNegociacao() {
 
-        let negociacao = new Negociacao(
-            data,
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
+    }
+    _limpaFormulario() {
 
-        console.log(negociacao);
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 }
